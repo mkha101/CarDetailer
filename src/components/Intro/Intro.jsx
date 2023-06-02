ca540be25e957284ca5305ee5fc1ead00f0275a2
muzamilkhan <k.muzamil1001@gmail.com>
2023-06-02 01:35:09 -0400

    first commit

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Intro.css";
+
+function Intro() {
+  return (
+    <div className="intro">
+      <div className="hello">
+        <h1 className="hi">
+          Plan your next <span>Adventure</span> today!
+        </h1>
+
+        <button class="button-24" role="button">
+          Start{" "}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
