37cc084d725868e62f765f1b358754ae4840d73e
muzamilkhan <k.muzamil1001@gmail.com>
2023-06-02 14:57:15 -0400

    Layout complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,26 +4,10 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <div className="container">
-        <div className="logo">
-          <h1>
-            <a href="/">ItineraryGen</a>
-          </h1>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-
-            <li>
-              <a href="/">Contact</a>
-            </li>
-          </ul>
-        </nav>
+      <div className="logo">
+        <h1>
+          <a href="/">ItineraryGen</a>
+        </h1>
       </div>
     </header>
   );
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,19 +1,50 @@
 import React from "react";
 import "./Intro.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 function Intro() {
-  return (
-    <div className="intro">
-      <div className="hello">
-        <h1 className="hi">
-          Plan your next <span>Adventure</span> today!
-        </h1>
+  const [selectedDate, setSelectedDate] = useState(null);
 
-        <button class="button-24" role="button">
-          Start{" "}
-        </button>
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+  return (
+    <section id="intro">
+      <div className="intro_container">
+        <div className="hello">
+          <h1 className="hi">
+            Plan <br />
+            Your Next <span>Adventure</span>!
+          </h1>
+          <div className="round-container">
+            <div className="left-container">
+              <input
+                type="text"
+                className="round-textfield"
+                placeholder="Location"
+              />
+            </div>
+            <div className="right-container">
+              <DatePicker
+                className="round-datepicker"
+                selected={selectedDate}
+                onChange={handleDateChange}
+                placeholderText="Start Date"
+              />
+
+              <DatePicker
+                className="round-datepicker"
+                selected={selectedDate}
+                onChange={handleDateChange}
+                placeholderText="End Date"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
